9e9b258e99c6a7def6eda4de2d8eca95b1f0adbd
shahjalal132 <rjshahjalal132@gmail.com>
2024-07-03 09:49:12 +0600

    added poa_create_order_status_table

---
+++ inc/woo-create-tables.php
@@ -0,0 +1,24 @@
+<?php
+
+// Create sync_order_status Table When Plugin Activated
+function poa_create_order_status_table() {
+
+    global $wpdb;
+
+    $table_name      = $wpdb->prefix . 'sync_order_status';
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+        id INT AUTO_INCREMENT,
+        order_id INT NOT NULL,
+        order_data TEXT NULL,
+        order_status VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+        PRIMARY KEY (id)
+    ) $charset_collate;";
+
+    require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+    dbDelta( $sql );
+}
