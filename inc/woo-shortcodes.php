c44d003730b2deb7fec5cc58853fea7ce8c40f63
shahjalal132 <rjshahjalal132@gmail.com>
2024-07-03 13:30:25 +0600

    update woo order status programmatically

--- inc/woo-shortcodes.php
+++ inc/woo-shortcodes.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Update Order data to DB
+ *
+ * @return string
+ */
 function poa_update_order_status_db() {
 
     $data_file_path = WOO_ORDER_API_PLUGIN_PATH . '/api_response/order-data.json';
@@ -12,3 +17,47 @@ function poa_update_order_status_db() {
 
 }
 add_shortcode( 'update_order_status', 'poa_update_order_status_db' );
+
+
+/**
+ * Get Orders from DB
+ *
+ */
+function get_order_status_from_db_callback() {
+
+    global $wpdb;
+    $table_name   = $wpdb->prefix . 'sync_order_status';
+    $order_status = $wpdb->get_results( "SELECT id, order_id, order_status FROM $table_name" );
+
+    return $order_status;
+    // echo '<pre>';
+    // print_r( $order_status );
+}
+
+add_shortcode( 'get_order_status_from_db', 'get_order_status_from_db_callback' );
+
+
+function update_woo_order_status_callback() {
+
+    $order_status_from_db = get_order_status_from_db_callback();
+
+    if ( !empty( $order_status_from_db ) && is_array( $order_status_from_db ) ) {
+
+        foreach ( $order_status_from_db as $key => $value ) {
+
+            $order_id   = $value->order_id;
+            $new_status = $value->order_status;
+
+            // Update the status
+            if ( $order_id ) {
+                $order = wc_get_order( $order_id );
+                if ( $order ) {
+                    $order->update_status( $new_status, 'Order status updated by api', true );
+                }
+            }
+        }
+    }
+
+}
+
+add_shortcode( 'update_woo_order_status', 'update_woo_order_status_callback' );
--- inc/woo-update-woo-order-status.php
+++ inc/woo-update-woo-order-status.php
@@ -1,2 +1,12 @@
 <?php
 
+function poa_update_woo_order_status( $order_id, $new_status ) {
+
+    // Get an instance of the WC_Order object
+    $order = wc_get_order( $order_id );
+
+    // Update the status
+    if ( $order ) {
+        $order->update_status( $new_status, 'Order status updated based on the API', true );
+    }
+}
